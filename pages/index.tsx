bbd52b1aaa6bc7644285b6578d50c935c269b696
AlexTraher <alex.traher@gmail.com>
2021-06-23 17:11:04 +0100

    Remove unused styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useCallback, useState } from 'react';
 import { Form } from '../client/Form';
 import { IProcessDataResponse } from '../types';
